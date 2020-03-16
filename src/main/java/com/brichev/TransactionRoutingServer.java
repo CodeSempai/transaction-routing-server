b9aeaf5d4a58876179c00670ba808492f24d6f91
CodeSempai <tony.bricheff@yandex.ru>
2020-03-16 18:11:42 +0300

    init

---
+++ src/main/java/com/brichev/TransactionRoutingServer.java
@@ -0,0 +1,13 @@
+package com.brichev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRoutingServer {
+
+    public static void main(String[] args){
+        SpringApplication.run(TransactionRoutingServer.class, args);
+    }
+
+}
