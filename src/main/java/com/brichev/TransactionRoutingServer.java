87e0aac781ac78279f32cf47dc0b6c323a788077
tonybricheff <tony.bricheff@yandex.ru>
2020-04-06 22:42:43 +0300

    init database

--- src/main/java/com/brichev/TransactionRoutingServer.java
+++ src/main/java/com/brichev/TransactionRoutingServer.java
@@ -1,14 +1,8 @@
 package com.brichev;
 
-import com.brichev.templates.CardForm;
-import com.mongodb.client.MongoClients;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+
 
 @SpringBootApplication
 public class TransactionRoutingServer {
---
+++ src/main/java/com/brichev/controllers/DatabaseController.java
@@ -0,0 +1,30 @@
+package com.brichev.controllers;
+
+import com.brichev.repositories.AccordanceRepository;
+import com.brichev.templates.Accordance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/db")
+public class DatabaseController {
+
+    @Autowired
+    private AccordanceRepository accordanceRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUAccordance(@RequestParam String bin, @RequestParam String url, @RequestParam String targetName) {
+        Accordance accordance = new Accordance();
+        accordance.setBin(bin);
+        accordance.setUrl(url);
+        accordance.setTargetName(targetName);
+        accordanceRepository.save(accordance);
+        return "Saved";
+    }
+
+}
