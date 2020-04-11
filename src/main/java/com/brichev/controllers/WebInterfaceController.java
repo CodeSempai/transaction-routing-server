a4ebb55f32d2fe8c2e431e3112b9433bad5c2438
tonybricheff <tony.bricheff@yandex.ru>
2020-04-11 21:58:45 +0300

    added web interface controller

---
+++ src/main/java/com/brichev/controllers/WebInterfaceController.java
@@ -0,0 +1,21 @@
+package com.brichev.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class WebInterfaceController {
+
+    /*
+    @RequestMapping(path = "/")
+    public ModelAndView test() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+*/
+
+
+}
