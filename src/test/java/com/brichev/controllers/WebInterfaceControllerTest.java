15735133844f35ac642d6049d756ab626d61d7e7
tonybricheff <tony.bricheff@yandex.ru>
2020-04-21 13:56:58 +0300

    WebInterfaceController test added

--- src/test/java/com/brichev/controllers/WebInterfaceControllerTest.java
+++ src/test/java/com/brichev/controllers/WebInterfaceControllerTest.java
@@ -2,10 +2,10 @@ package com.brichev.controllers;
 
 import com.brichev.models.BankIssuer;
 import com.brichev.models.BankIssuerBin;
+import com.brichev.models.WebForm;
 import com.brichev.repositories.BankIssuerBinRepository;
 import com.brichev.repositories.BankIssuerRepository;
 import com.brichev.services.BankIssuerService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +14,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(BankIssuerController.class)
-class BankIssuerControllerTest {
+@WebMvcTest(WebInterfaceController.class)
+class WebInterfaceControllerTest {
+
     @Autowired
     private MockMvc mvc;
     @MockBean
@@ -36,52 +35,38 @@ class BankIssuerControllerTest {
     BankIssuerRepository bankIssuerRepository;
 
     private final BankIssuerBin bankIssuerBin;
+    private final WebForm webForm;
 
-    BankIssuerControllerTest(){
+    WebInterfaceControllerTest(){
         bankIssuerBin = new BankIssuerBin();
         bankIssuerBin.setBin("12345");
         BankIssuer bankIssuer = new BankIssuer();
         bankIssuer.setUrl("www.test.com");
         bankIssuer.setTargetName("test");
         bankIssuerBin.setBankIssuer(bankIssuer);
+
+        webForm = new WebForm(bankIssuerBin);
     }
 
     @Test
-    void addNewAccordance() throws Exception {
-        this.mvc.perform(post("/add")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(toJsonString((bankIssuerBin))))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void removeAccordance() throws Exception {
-        List <BankIssuer> bankIssuerList = new ArrayList<>();
+    void binsList() throws Exception {
+        List<BankIssuer> bankIssuerList = new ArrayList<>();
         bankIssuerList.add(bankIssuerBin.getBankIssuer());
         given(bankIssuerBinRepository.findById(bankIssuerBin.getId())).willReturn(java.util.Optional.of(new BankIssuerBin()));
         given(bankIssuerRepository.findByUrlAndAndTargetName(bankIssuerBin.getBankIssuer().getUrl(),
                 bankIssuerBin.getBankIssuer().getTargetName())).willReturn(bankIssuerList);
-        this.mvc.perform(post("/delete")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(toJsonString((bankIssuerBin))))
-                .andExpect(status().isOk());
+        mvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Test
-    void getBins() throws Exception {
-        given(bankIssuerService.getAllBins()).willReturn(new ArrayList<>());
-        mvc.perform(get("/getBins"))
+    void showCreatePage() throws Exception {
+        mvc.perform(get("/create"))
                 .andExpect(status().isOk())
                 .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+                        .contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
-
-    public static String toJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
